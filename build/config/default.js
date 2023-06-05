d632d67c3c678e6ea947b596344fc37e15c4fc47
Clement Omolayo <naijawebmaster1@gmail.com>
2023-06-05 22:24:15 +0100

    deleted build file
    deleted build file

--- build/config/default.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    origin: 'http://localhost:3000',
-    accessTokenExpiresIn: 15,
-    refreshTokenExpiresIn: 60,
-    redisCacheExpiresIn: 60,
-    emailFrom: 'contact@tusenti-backend-engineer-test.com',
-};
